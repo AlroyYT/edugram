6be08239bbc337d004b294f90d330e0944020e66
Peter <peterpaul731@gmail.com>
2024-12-19 18:03:20 +0530

    maja

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,14 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+const id=process.env.GOOGLE_CLIENT_ID;
+const secret=process.env.GOOGLE_CLIENT_SECRET;
+
 export default NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: id,
+      clientSecret: secret,
     }),
   ],
 
