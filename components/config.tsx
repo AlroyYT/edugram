3cc3d24ffe5ecc34683fc4072d8b2cfbfa074516
unknown <alroyvader123@gmail.com>
2025-07-26 10:19:15 +0530

    ltm

--- components/config.tsx
+++ components/config.tsx
@@ -1,1 +1,3 @@
-export const backend_url="https://edugram-574544346633.asia-south1.run.app/"
+//export const backend_url="https://edugram-574544346633.asia-south1.run.app/"
+
+export const backend_url="http://localhost:8000/"
