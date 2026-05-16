d2d797a5714576cf63e6bcaab77de6084182e4b5
unknown <alroyvader123@gmail.com>
2026-05-17 00:15:05 +0530

    yt

--- components/config.tsx
+++ components/config.tsx
@@ -1,5 +1,5 @@
 //export const backend_url="https://edugram-574544346633.asia-south1.run.app/"
 
 //export const backend_url="https://edugram-408537457754.asia-south1.run.app"
-export const backend_url="http://127.0.0.1:8000"
-//export const backend_url="https://stacksmashers.online"
+//export const backend_url="http://127.0.0.1:8000"
+export const backend_url="https://stacksmashers.online"
